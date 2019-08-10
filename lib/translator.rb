b17e7f55309a9ed4836c78e47d3e12e679d31669
Marco McNulty <mmc90@hotmail.co.uk>
2019-08-10 17:47:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,31 +24,3 @@ def get_english_meaning(file_path, emoticon)
   return "Sorry, that emoticon was not found" if emoji_equivalent.nil?
   emoji_equivalent
 end
-
-
-
-
-# {
-#   "angel"=>["O:)", "☜(⌒▽⌒)☞"], 
-#   "angry"=>[">:(", "ヽ(ｏ`皿′ｏ)ﾉ"], 
-#   "bored"=>[":O", "(ΘεΘ;)"], 
-#   "confused"=>["%)", "(゜.゜)"], 
-#   "embarrased"=>[":$", "(#^.^#)"], 
-#   "fish"=>["><>", ">゜))))彡"], 
-#   "glasses"=>["8D", "(^0_0^)"], 
-#   "grinning"=>["=D", "(￣ー￣)"], 
-#   "happy"=>[":)", "(＾ｖ＾)"], 
-#   "kiss"=>[":*", "(*^3^)/~☆"], 
-#   "sad"=>[":'(", "(Ｔ▽Ｔ)"], 
-#   "surprised"=>[":o", "o_O"], 
-#   "wink"=>[";)", "(^_-)"]
-# }
-
-  # japanese_emoticons = []
-  # western_emoticons = []
-  # emoji_meanings = []
-  # emojis.each do |key, value|
-  #   western_emoticons << value.first
-  #   japanese_emoticons << value.last
-  #   emoji_meanings << key
-  # end
