ef699f3ec0f99aa431c24b32459e05f48fc42484
Marco McNulty <mmc90@hotmail.co.uk>
2019-08-10 16:14:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,7 @@ require "yaml"
 
 def load_library(file_path)
   emojis = YAML.load_file(file_path)
-  p emojis
+  emojis
 end
 
 def get_japanese_emoticon
@@ -16,18 +16,18 @@ end
 
 
 
-{
-  "angel"=>["O:)", "☜(⌒▽⌒)☞"], 
-  "angry"=>[">:(", "ヽ(ｏ`皿′ｏ)ﾉ"], 
-  "bored"=>[":O", "(ΘεΘ;)"], 
-  "confused"=>["%)", "(゜.゜)"], 
-  "embarrased"=>[":$", "(#^.^#)"], 
-  "fish"=>["><>", ">゜))))彡"], 
-  "glasses"=>["8D", "(^0_0^)"], 
-  "grinning"=>["=D", "(￣ー￣)"], 
-  "happy"=>[":)", "(＾ｖ＾)"], 
-  "kiss"=>[":*", "(*^3^)/~☆"], 
-  "sad"=>[":'(", "(Ｔ▽Ｔ)"], 
-  "surprised"=>[":o", "o_O"], 
-  "wink"=>[";)", "(^_-)"]
-}
+# {
+#   "angel"=>["O:)", "☜(⌒▽⌒)☞"], 
+#   "angry"=>[">:(", "ヽ(ｏ`皿′ｏ)ﾉ"], 
+#   "bored"=>[":O", "(ΘεΘ;)"], 
+#   "confused"=>["%)", "(゜.゜)"], 
+#   "embarrased"=>[":$", "(#^.^#)"], 
+#   "fish"=>["><>", ">゜))))彡"], 
+#   "glasses"=>["8D", "(^0_0^)"], 
+#   "grinning"=>["=D", "(￣ー￣)"], 
+#   "happy"=>[":)", "(＾ｖ＾)"], 
+#   "kiss"=>[":*", "(*^3^)/~☆"], 
+#   "sad"=>[":'(", "(Ｔ▽Ｔ)"], 
+#   "surprised"=>[":o", "o_O"], 
+#   "wink"=>[";)", "(^_-)"]
+# }
